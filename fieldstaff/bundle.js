23faefdd33f0014b49de42cf3a80c978248a6443
Johannes Hilden <hildenjohannes@gmail.com>
2015-07-06 02:05:52 +0200

    snapshot

--- fieldstaff/bundle.js
+++ fieldstaff/bundle.js
@@ -2673,8 +2673,8 @@ var api = new GroundFork.Api({
 
 var endpoint = new GroundFork.BasicHttpEndpoint({
     api: api,
-    //url: "http://agile-oasis-7393.herokuapp.com/",
-    url: "http://localhost:3333/",
+    url: "http://agile-oasis-7393.herokuapp.com/",
+    //url: "http://localhost:3333/",
     clientKey: "fieldstaff-user1",
     clientSecret: "fieldstaff",
     onRequestStart: function() {},
